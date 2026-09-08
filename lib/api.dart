911c4a5d3faace1a06f1c6bcc9d163cc60871739
kayysan <jokyo931@gmail.com>
2026-09-09 06:37:13 +0700

    fix comment

--- lib/api.dart
+++ lib/api.dart
@@ -1,5 +1,1 @@
-// Ganti sesuai tempat jalanin frontend:
-// - Chrome / Windows desktop: http://localhost:8000/api
-// - Android emulator: http://10.0.2.2:8000/api
-// - HP fisik (WiFi sama kayak laptop): http://192.168.1.11:8000/api
 const String baseUrl = 'http://localhost:8000/api';
